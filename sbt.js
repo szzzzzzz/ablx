94248a285040a23bd4e2fcab5df314350518a2f8
szzzzzzz <szzz8246@gmail.com>
2014-04-19 18:39:39 +0800

    19/4

--- sbt.js
+++ sbt.js
@@ -3,18 +3,17 @@ window.onload = function dat() {
     document.getElementById("l1").value = localStorage.alx || "";
     document.getElementById("la").value = localStorage.ala || "2/3【KOF】";
 };
-function sbtt($scope) {
-    var a1 = ra(document.getElementById("l3").value.split(' ')),
-        a2 = ra(document.getElementById("l4").value.split(' '));
-    $scope.sList=[a1,a2]
-    $scope.ssub=function(){sub()}
-};
-    function bb(x) {
+function bb(x) {
     var b = "";
     for (var i = 12 - x.replace(/[^\x00-\xff]/g, "**").length; i > 0; i--) {
         b += "\u00A0\u00A0"
     }
     return b
+}
+function room(x, y, y1, y2) {
+    var ttx = 'x' + y;
+    document.getElementById(ttx).innerHTML = y1 + bb(y1) + x + bb(y2) + y2;
+
 }
 function sub() {
     var tx2 = "",
@@ -31,9 +30,7 @@ function sub() {
 
         for (var i = a1.length - 1; i >= 0; i--) {
             tx2 += '<p id="x' + i + '">'
-                + a1[i] + bb(a1[i]) + "0:0" + bb(a2[i]) + a2[i]
-                + '\u00A0\u00A0<a href="mycard://122.0.65.69:7911/M%23' + document.getElementById("l1").value
-                + document.getElementById("l2").value + i + '">room</a>'+ '<p/>'
+                + a1[i] + bb(a1[i]) + "0:0" + bb(a2[i]) + a2[i]+ '<p/>'
         }
         ;
         document.getElementById("mes").innerHTML = txt + tx2 + "------------第二轮------------";
--- spt.js
+++ spt.js
@@ -1,41 +1,55 @@
 function sbtt($scope) {
-    $scope.date= new Date().toJSON().slice(0, 10)
-    $scope.l1= localStorage.alx || ""
-    $scope.la   = localStorage.ala || "2/3【KOF】"
-    $scope.lb   = localStorage.alb || ""
-
-    $scope.ssub=function(){
-        $scope.sList=sub();
+    $scope.date = new Date().toJSON().slice(0, 10)
+    $scope.l1 = localStorage.alx || ""
+    $scope.la = localStorage.ala || "2/3【KOF】"
+    $scope.lb = localStorage.alb || ""
+    $scope.l3 = $scope.l3 ||""
+    $scope.l4 = $scope.l4 ||""
+    $scope.ss = [1,2];
+    $scope.s = [];
+    $scope.sl1 = [];
+    $scope.sl2 = [];
+    $scope.ssub = function () {
+        $scope.sl1[0] = ra($scope.l3.split(' '));
+        $scope.sl2[0] = ra($scope.l4.split(' '));
+        $scope.s[0] = $scope.sl1[0]
+    };
+    $scope.act = function (j,k,r) {
+        if ($scope.sl1[r][k] && $scope.sl2[r][k]){
+        if (r+2 > $scope.ss.length) {
+            $scope.ss.push(r+2)
         }
-    $scope.act=function(j,k){
-        $scope.s2List=[[k[0],k[1]]];
-    }
-};
-    function bb(x) {
+        $scope.sl1[r+1]=$scope.sl1[r+1]||[]
+        $scope.sl2[r+1]=$scope.sl2[r+1]||[]
+        delfo($scope.sl1[r+1], $scope.sl1[r][k]);
+        delfo($scope.sl2[r+1], $scope.sl2[r][k]);
+        if (j[0] == "2") {
+            $scope.sl1[r+1].push($scope.sl1[r][k])
+        }
+        else if (j[2] == "2") {
+            $scope.sl2[r+1].push($scope.sl2[r][k])
+        } else {
+        }
+        if ($scope.sl2[r+1].length > $scope.sl1[r+1].length) {
+            $scope.s[r+1] = $scope.sl2[r+1]
+
+        } else {
+            $scope.s[r+1] = $scope.sl1[r+1]
+        }
+    }}
+}
+function bb(x) {
     var b = "";
     for (var i = 12 - x.replace(/[^\x00-\xff]/g, "**").length; i > 0; i--) {
         b += "\u00A0\u00A0"
     }
     return b
 }
-function sub() {
-    var tx={},
-        a1 = ra(document.getElementById("l3").value.split(' ')),
-        a2 = ra(document.getElementById("l4").value.split(' '));
-    if (a1.length == a2.length) {
-        for (var i = a1.length - 1; i >= 0; i--) {
-            tx[i]=[a1[i],a2[i]]
-        }
-        localStorage.alx = document.getElementById("l1").value;
-        localStorage.ala = document.getElementById("la").value;
-        localStorage.alb = document.getElementById("lb").value;
-
-        return tx
-    } else {
-        alert("2队人数不同！")
-    };
-}
 function ra(dad) {
-        for (var j, x, i = dad.length; i; j = parseInt(Math.random() * i), x = dad[--i], dad[i] = dad[j], dad[j] = x);
-        return dad;
-    };
+    for (var j, x, i = dad.length; i; j = parseInt(Math.random() * i), x = dad[--i], dad[i] = dad[j], dad[j] = x);
+    return dad;
+};
+function delfo(b, a) {
+    for (var i = b.length; i-- && b[i] !== a;);
+    if (i >= 0) b.splice(i, 1);
+}
